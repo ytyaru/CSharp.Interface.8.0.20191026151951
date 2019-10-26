f2492ad650804968fd094d72d4d21db3ecae9a46
ytyaru <ytyaru@outlook.jp>
2019-10-26 15:21:14 +0900

    初回コミット

---
+++ src/CS8_Interface/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CS8_Interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(new DefaultBot().Generate());
+            Console.WriteLine(new FixedBot().Generate());
+            Console.WriteLine(new DateTimeBot().Generate());
+            Console.WriteLine(new GreetingBot().Generate());
+        }
+    }
+}
